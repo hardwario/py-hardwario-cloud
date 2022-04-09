c87f22bf58b9a7eb60710f49c3241b283bc37dfb
Karel Blavka <karel.blavka@hardwario.com>
2022-04-09 08:17:31 +0200

    Fix required option token for --help

--- src/hardwario/cloud/cli/__init__.py
+++ src/hardwario/cloud/cli/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 import click
 from . import codec
 from ..api import Api, DEFAULT_API_URL
@@ -8,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 @click.group(name='cloud', help='Commands for CLOUD.')
 @click.option('--url', metavar='URL', default=DEFAULT_API_URL, show_default=True, required=True)
-@click.option('--token', metavar='TOKEN', required=True)
+@click.option('--token', metavar='TOKEN', required='--help' not in sys.argv)
 @click.pass_context
 def cli(ctx, url, token):
     ctx.obj['api'] = Api(url, token=token)
