c5768e3a4118a34ce3b9bc58baac387422e661ac
Karel Blavka <karel.blavka@hardwario.com>
2022-04-27 12:14:10 +0200

    Update api

--- src/hardwario/cloud/api.py
+++ src/hardwario/cloud/api.py
@@ -1,4 +1,5 @@
 import requests
+from loguru import logger
 
 DEFAULT_API_URL = 'https://api.hardwario.cloud'
 
@@ -18,14 +19,16 @@ class Api:
     def set_token(self, token):
         self._headers['Authorization'] = 'Bearer ' + token
 
-    def request(self, method, expect_code, url, **kwargs):
+    def request(self, method, url, **kwargs):
+        url = self._url + url
+        logger.debug('{} {}', url, kwargs)
         try:
             self._response = requests.request(
-                method, self._url + url, headers=self._headers, **kwargs)
+                method, url, headers=self._headers, **kwargs)
         except ConnectionError:
             raise ApiException('Cannot connect to cloud service')
 
-        if expect_code and expect_code != self._response.status_code:
+        if 200 < self._response.status_code >= 300:
             text = self._response.text.strip('"')
             raise ApiException(f'{self._response.status_code}: {text}')
 
@@ -39,9 +42,9 @@ class Api:
         params['offset'] = offset
 
         while True:
-            params['limit'] = 2 if limit is None else limit
+            params['limit'] = 100 if limit is None else limit
 
-            for row in self.request('GET', 200, url, params=params):
+            for row in self.request('GET', url, params=params):
                 cnt += 1
                 yield row
 
@@ -58,7 +61,7 @@ class Api:
         body = {'name': name}
         if note:
             body['note'] = note
-        return self.request('POST', 200, '/v1/codecs', json=body)
+        return self.request('POST', '/v1/codecs', json=body)
 
     def codec_update(self, id: str, name: str = None, note: str = None, decoder_type: str = None, decoder: str = None):
         body = {}
@@ -71,7 +74,7 @@ class Api:
         if decoder:
             body['decoder'] = decoder
 
-        return self.request('PUT', 200, f'/v1/codec/{id}', json=body)
+        return self.request('PUT', f'/v1/codec/{id}', json=body)
 
     def codec_list(self, fields: list = None, offset: int = 0, limit: int = None):
         params = {}
@@ -80,13 +83,13 @@ class Api:
         return self._list('/v1/codecs', params, offset, limit)
 
     def codec_detail(self, id):
-        return self.request('GET', 200, f'/v1/codec/{id}')
+        return self.request('GET', f'/v1/codec/{id}')
 
     def codec_delete(self, id):
-        return self.request('DELETE', 200, f'/v1/codec/{id}')
+        return self.request('DELETE', f'/v1/codec/{id}')
 
     def codec_attach_to_device(self, id, device_id):
-        return self.request('PUT', 200, f'/v1/device/{device_id}', json={'codec_id': id})
+        return self.request('PUT', f'/v1/device/{device_id}', json={'codec_id': id})
 
     def codec_attach_to_group(self, id, group_id):
-        return self.request('PUT', 200, f'/v1/group/{group_id}', json={'codec_id': id})
+        return self.request('PUT', f'/v1/group/{group_id}', json={'codec_id': id})
